7933d50bb98fce71a0fc26f5b75b3871125c23af
rfmarves <mar23032@byui.edu>
2024-09-26 00:23:30 -0600

    started structure for chamber of commerce site

---
+++ chamber/scripts/main.js
@@ -0,0 +1,20 @@
+// DOM elements
+// const mainnav = document.querySelector("#animateme");
+// const hambutton = document.querySelector("#menu");
+const currentYearElement = document.querySelector("#currentYear");
+const lastModifiedElement = document.querySelector("#lastModified");
+
+// hambutton.addEventListener('click', () => {
+// 	mainnav.classList.toggle('show');
+// 	hambutton.classList.toggle('show');
+// });
+
+// gets the date and year
+const today = new Date();
+let currentYear = today.getFullYear();
+
+// writes current year into the corresponding SPAN element
+currentYearElement.innerHTML = currentYear;
+
+// gets and writes when this document was last modified
+lastModifiedElement.innerHTML = document.lastModified;
