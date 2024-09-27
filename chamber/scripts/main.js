e24a04f9a62b31f71579b2c13379fcaec0e203bd
rfmarves <mar23032@byui.edu>
2024-09-26 23:51:12 -0600

    added dark mode

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,6 +3,9 @@ const mainnav = document.querySelector(".navigation");
 const hambutton = document.querySelector("#menu");
 const currentYearElement = document.querySelector("#currentYear");
 const lastModifiedElement = document.querySelector("#lastModified");
+const darkmode1 = document.querySelector("#themetoggle-desk");
+const darkmode2 = document.querySelector("#themetoggle-mobile");
+const bodyTag = document.querySelector("body");
 
 hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
@@ -18,3 +21,11 @@ currentYearElement.innerHTML = currentYear;
 
 // gets and writes when this document was last modified
 lastModifiedElement.innerHTML = document.lastModified;
+
+darkmode1.addEventListener('click', darkModeTriggered);
+darkmode2.addEventListener('click', darkModeTriggered);
+
+
+function darkModeTriggered() {
+    bodyTag.classList.toggle("dark-mode");
+}
