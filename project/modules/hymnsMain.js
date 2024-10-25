4cff43b1513cdfadd1c25f26f1770dc2abba8d03
rfmarves <mar23032@byui.edu>
2024-10-25 10:23:51 -0600

    commenting cleanup and folder restructure

--- project/modules/hymnsMain.js
+++ project/modules/hymnsMain.js
@@ -2,13 +2,11 @@ import hymnsDataJson from './getHymns.mjs';
 const hymnsData = hymnsDataJson.hymns;
 import { HymnList } from './hymnList.mjs';
 
-// DOM objects
+// DOM identifiers
 const modalId = "hymn-details-modal";
 const containerId = "hymn-block";
 const cardClass = "card";
 const hymnTypeSelector = 'input[name="hymn-type"]:checked';
-const hymnModal = document.getElementById("hymn-details-modal");
-const typeSelection = document.querySelector('input[name="hymn-type"]:checked').value;
 
 // Create HymnList object
 const hymns = new HymnList();
